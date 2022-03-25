bc2ac36e1d2c93ad7d96bfd6af88e5576640c944
huseyin <hsyndg7@gmail.com>
2022-03-25 13:31:20 +0300

    first commit

---
+++ hoaxify/src/main/java/com/hoaxify/hoaxif_project/HoaxifProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hoaxify.hoaxif_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoaxifProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoaxifProjectApplication.class, args);
+    }
+
+}
---
+++ hoaxify/src/test/java/com/hoaxify/hoaxif_project/HoaxifProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoaxify.hoaxif_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoaxifProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
